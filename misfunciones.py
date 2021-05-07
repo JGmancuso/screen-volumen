317e15b407b0d56e9842394d3ffdf3bcd105fe5a
JGmancuso <61671037+JGmancuso@users.noreply.github.com>
2021-05-07 15:49:59 -0300

    Update misfunciones.py

--- misfunciones.py
+++ misfunciones.py
@@ -126,6 +126,8 @@ def volacumrango(inicio,fin,activo):
   return volumen 
 
 def matriztrabajo(tickers,start,end):
+  import yfinance as yf
+  
   sd = start
   ed = end
   tickers= tickers# funcion que determine los ticker automaticamente
