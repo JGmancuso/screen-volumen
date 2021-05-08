3946b3c4226269a65d3119a12779519df3492519
JGmancuso <61671037+JGmancuso@users.noreply.github.com>
2021-05-08 18:28:59 -0300

    Update misfunciones.py

--- misfunciones.py
+++ misfunciones.py
@@ -16,7 +16,7 @@ from bs4 import BeautifulSoup
 import requests
 from datetime import datetime
 import mplfinance as mpf
-#import yfinance as yf
+import yfinance as yf
 import ta
 import datetime
 import os.path
