3c502d7198a522f0df8af54e90a40cc877b6aaa3
JGmancuso <61671037+JGmancuso@users.noreply.github.com>
2023-04-15 12:28:47 -0300

    Update misfunciones.py

--- misfunciones.py
+++ misfunciones.py
@@ -1196,47 +1196,6 @@ class Volumen():
     self.activos=grupo_activo_volumen(data.data)
     
     
-def beta(df,periodo=24):
-  '''
-  Calculo semanal.
-  El calculo se realiza en fracciones de medio año por eso 24 semanas.
-  DF es la matriz de retornos
-  '''
-
-  act=df.rolling(periodo).cov()
-  #pre=df2.rolling(periodo).corr()
-  info=pd.DataFrame(index=act.loc[(slice(None),'Indice'),slice(None)].mean().index) # Listado de todos los activos como indice.
-  info['CovMedia']=act.loc[(slice(None),'Indice'),slice(None)].mean().values
-
-  info=info.join(act.loc[(act.index[-1],'Indice'),slice(None)].T.droplevel(0,axis=1),how='left')
-
-  info.rename(columns={'Indice':'CovActual'},inplace=True)
-
-  sigmaindice=(df['Indice'].rolling(periodo).std())**2
-
-
-  beta=pd.DataFrame(np.zeros_like(act.loc[(slice(None),'Indice'),slice(None)]),columns=act.columns, index=sigmaindice.index)
-
-  for i in beta.columns:
-    beta.loc[:,i]=act.loc[(slice(None),'Indice'),i].values/sigmaindice.values
-  beta
-
-  info['BetaActual']=beta.iloc[-1]
-  info['BetaMedia']=beta.mean()
-
-  info['Tipo_Beta']=0
-
-  for i in info.index:
-    if info.loc[i]['BetaMedia']>=-1 and info.loc[i]['BetaMedia']<=0:
-      info.loc[i,'Tipo_Beta']='Inversa'
-    elif info.loc[i]['BetaMedia']>0 and info.loc[i]['BetaMedia']<=0.8:
-      info.loc[i,'Tipo_Beta']='Defensiva'
-    elif info.loc[i]['BetaMedia']>0.8 and info.loc[i]['BetaMedia']<=1.01:
-      info.loc[i,'Tipo_Beta']='Agresiva'
-    elif info.loc[i]['BetaMedia']>1.01:
-      info.loc[i,'Tipo_Beta']='Muy Agresiva'
-
-  return info.iloc[:,-3:]
 
 def comportamiento_activos(df,corr,Beta,RS,Activos='todos'):
   '''
